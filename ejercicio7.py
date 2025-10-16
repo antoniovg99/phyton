18b79c10b028052c9b2cc9289f13351ffd563288
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-16 10:05:19 +0200

    ejercicio7

---
+++ ejercicio7.py
@@ -0,0 +1,4 @@
+peso = float(input("Ingrese su peso en kg: "))
+estatura = float(input("Ingrese su estatura en metros: "))
+imc = (peso / (estatura * estatura))
+print("Su indice de masa corporal es: ", imc)
