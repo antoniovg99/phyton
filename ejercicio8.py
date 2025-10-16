81760157f69ca946c66f3849de1d23693cf7056f
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-16 11:08:45 +0200

    ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese un numero entero: "))
+numero2 = int(input("Ingrese otro numero entero: "))
+cociente = numero1 // numero2
+resto = numero1 % numero2
+
+print ("El cociente es: ", cociente)
+print ("El resto es: ", resto)
