bf813afcdcd2d12f825cf81336380b71954381eb
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-23 10:20:36 +0200

    ejercicio4

---
+++ cadenaCaracteres/ejercicio4.py
@@ -0,0 +1,2 @@
+telefono = input ("Introduce el numero de telefono con su prefijo y extension: ")
+print (telefono[4:13])
---
+++ cadenaCaracteres/ejercicio7.py
@@ -0,0 +1,5 @@
+gmail = input ("Introduce tu email: ")
+arroba = gmail.split("@")
+nombre = arroba[0]
+print (nombre+"@ceu.es")
+
