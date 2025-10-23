a03ea3fc41c9004f40a0bef0e889561271f0ec74
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-23 10:39:20 +0200

    ejercicio9

---
+++ cadenaCaracteres/ejercicio9.py
@@ -0,0 +1,6 @@
+fechaNacimiento = input("Introduce tu fecha de nacimiento (dd/mm/aaaa): ")
+fechaSplit = fechaNacimiento.split("/")
+
+print (fechaSplit)
+print("Dia: ", fechaSplit[0], "Mes: ", fechaSplit[1], "Año: ", fechaSplit[2])
+
