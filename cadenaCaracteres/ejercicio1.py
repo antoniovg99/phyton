89008fadb3891e5fc49b94106a5ed9285000d6e5
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-23 09:35:34 +0200

    ejercicio1

---
+++ cadenaCaracteres/ejercicio1.py
@@ -0,0 +1,3 @@
+usuario = input("Introduce su nombre de usuario: ")
+numero = int(input("Introduce la cantidad de veces que quieras que muestre el nombre de usuario: "))
+print((usuario + "\n") * numero)
