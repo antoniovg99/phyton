b5221c09967605ccb6d5e9ee57ae14dda377ffc0
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-24 14:35:36 +0200

    ejercicio2

---
+++ codicionales/ejercicio2.py
@@ -0,0 +1,6 @@
+contraseña = input("Introduce la contraseña: ")
+contraseñaCorrecta = "avilgom943"
+if contraseña == contraseñaCorrecta:
+    print("Contraseña correcta")
+else:
+    print("Contraseña incorrecta")
