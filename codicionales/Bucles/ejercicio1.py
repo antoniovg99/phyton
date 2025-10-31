6d852664f3e7ecdcadb8acd1dffcf203ae2a4939
Antonio <avilgom943@ieszaidinvergeles.org>
2025-10-31 14:32:51 +0100

    ejercicio2.py

---
+++ codicionales/Bucles/ejercicio1.py
@@ -0,0 +1,5 @@
+p = input("Ingresa una palabra: ")
+contador = 0
+while contador < 10 :
+    print (p)
+    contador = contador +1
---
+++ codicionales/Bucles/ejercicio2.py
@@ -0,0 +1,5 @@
+edad = int(input("Indica tu edad: "))
+contador = 0
+while contador <= edad:
+    contador = contador +1
+    print ("Ha cumplido:", contador, "años")
