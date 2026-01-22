4b426cc80c382995ff12cc5fc86cb5b516644bd7
Antonio <avilgom943@ieszaidinvergeles.org>
2026-01-22 11:52:39 +0100

    listas

--- listas_Tuplas/ejercicio3.py
+++ listas_Tuplas/ejercicio3.py
@@ -2,7 +2,7 @@ asignaturas=["Matmaticas", "Fisica", "Quimica", "Historia", "Lengua"]
 notas=[]
 
 for asig in asignaturas:
-    nota=input("Que nota has sacado en "+asig)
+    nota=input("Que nota has sacado en "+asig+": ")
     notas.append(nota)
 
 for asignaturas, notas in zip(asignaturas, notas):
